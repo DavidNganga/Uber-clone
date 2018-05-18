5468142094eecc2bad24752ecb7f6a8707f37c98
DavidNganga <ngashiedavid@gmail.com>
2018-05-18 14:10:38 +0300

    installed django-registrtion

--- uber/uber/settings.py
+++ uber/uber/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'driver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'driver'
+
 ]
 
 MIDDLEWARE = [
