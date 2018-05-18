38e48338fe3635a4b10626f83ed16d6c102743a9
DavidNganga <ngashiedavid@gmail.com>
2018-05-18 15:19:12 +0300

    creted login.html

--- uber/driver/urls.py
+++ uber/driver/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from . import views
 
 
 urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
